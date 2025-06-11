f59d37bf89f4f8130b2f7172ef0f9a6734d938ba
DevAfrica2024 <fabrice.mehouelley@africadigitalizer.com>
2025-06-11 18:09:04 +0100

    CI: création DB test avant migrations

---
+++ eslint.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+    es2021: true,
+    jest: true
+  },
+  extends: [
+    'eslint:recommended'
+  ],
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module'
+  },
+  rules: {
+    // Ajoutez vos règles personnalisées ici
+  }
+};
