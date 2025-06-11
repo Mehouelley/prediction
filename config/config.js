dae0f58d99194f1cefa2c64a9da89645638c9383
DevAfrica2024 <fabrice.mehouelley@africadigitalizer.com>
2025-06-11 18:01:48 +0100

    CI: création DB test avant migrations

--- config/config.js
+++ config/config.js
@@ -9,12 +9,9 @@ module.exports = {
     dialect: 'mysql'
   },
   test: {
-    username: process.env.DB_USER_TEST || process.env.DB_USER, // Utilise DB_USER_TEST si défini, sinon DB_USER
-    password: process.env.DB_PASSWORD_TEST || process.env.DB_PASSWORD,
-    database: process.env.DB_NAME_TEST || `${process.env.DB_NAME}_test`, // Suffixe _test par défaut
-    host: process.env.DB_HOST_TEST || process.env.DB_HOST,
-    dialect: 'mysql',
-    logging: false // Désactiver les logs SQL pour les tests
+    dialect: 'sqlite',
+    storage: ':memory:',
+    logging: false // Utiliser SQLite en mémoire pour les tests
   },
   production: {
     username: process.env.DB_USER,
